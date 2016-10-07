e116f910a21224ad968b88b5869a261ce96cc5ac
lombax5832 <iPodAlexei@gmail.com>
2016-10-07 17:06:32 -0400

    BankAccount header file

--- BankAccount.h
+++ BankAccount.h
@@ -1,1 +1,18 @@
 #pragma once
+class BankAccount {
+public:
+  BankAccount(double initialBalance = 0, int initialAccountNumber = 0);
+
+  double withdrawFunds(double toWithdraw);
+  void depositFunds(double toDeposit);
+
+  void setAccountNumber(int);
+
+  int getAccountNumber() const;
+  double getBalance() const;
+  
+  void printInfo() const;
+private:
+  int accountNumber;
+  double balance;
+};
